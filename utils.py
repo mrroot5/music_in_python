d7556c6acc6d6651365174d6bb44e2718209bf89
Adrian Garrido Garcia <agarrido@qdqmedia.com>
2021-03-23 12:41:35 +0100

    utils pep8 fix

--- utils.py
+++ utils.py
@@ -7,8 +7,9 @@ Utility functions for writing music in Python.
 """
 import numpy as np
 
+
 def get_piano_notes():
-    '''
+    """
     Get the frequency in hertz for all keys on a standard piano.
 
     Returns
@@ -16,24 +17,25 @@ def get_piano_notes():
     note_freqs : dict
         Mapping between note name and corresponding frequency.
 
-    '''
-    
+    """
+
     # White keys are in Uppercase and black keys (sharps) are in lowercase
-    octave = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B'] 
-    base_freq = 440 #Frequency of Note A4
-    keys = np.array([x+str(y) for y in range(0,9) for x in octave])
+    octave = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B']
+    base_freq = 440  # Frequency of Note A4
+    keys = np.array([x + str(y) for y in range(0, 9) for x in octave])
     # Trim to standard 88 keys
     start = np.where(keys == 'A0')[0][0]
     end = np.where(keys == 'C8')[0][0]
-    keys = keys[start:end+1]
-    
-    note_freqs = dict(zip(keys, [2**((n+1-49)/12)*base_freq for n in range(len(keys))]))
-    note_freqs[''] = 0.0 # stop
+    keys = keys[start:end + 1]
+
+    note_freqs = dict(zip(keys, [2 ** ((n + 1 - 49) / 12) * base_freq for n in range(len(keys))]))
+    note_freqs[''] = 0.0  # stop
     return note_freqs
 
+
 def get_sine_wave(frequency, duration, sample_rate=44100, amplitude=4096):
-    '''
-    Get pure sine wave. 
+    """
+    Get pure sine wave.
 
     Parameters
     ----------
@@ -51,14 +53,15 @@ def get_sine_wave(frequency, duration, sample_rate=44100, amplitude=4096):
     wave : TYPE
         DESCRIPTION.
 
-    '''
-    t = np.linspace(0, duration, int(sample_rate*duration))
-    wave = amplitude*np.sin(2*np.pi*frequency*t)
+    """
+    t = np.linspace(0, duration, int(sample_rate * duration))
+    wave = amplitude * np.sin(2 * np.pi * frequency * t)
     return wave
 
+
 def apply_overtones(frequency, duration, factor, sample_rate=44100, amplitude=4096):
-    '''
-    Return fundamental note with overtones applied. 
+    """
+    Return fundamental note with overtones applied.
 
     Parameters
     ----------
@@ -67,7 +70,7 @@ def apply_overtones(frequency, duration, factor, sample_rate=44100, amplitude=40
     duration : float
         Time in seconds.
     factor : list
-        List of floats as fraction of the fundamental amplitude for amplitudes 
+        List of floats as fraction of the fundamental amplitude for amplitudes
         of the overtones.
     sample_rate : int, optional
         Wav file sample rate. The default is 44100.
@@ -79,20 +82,21 @@ def apply_overtones(frequency, duration, factor, sample_rate=44100, amplitude=40
     fundamental : ndarray
         Output note of `float` type.
 
-    '''
-    assert abs(1-sum(factor)) < 1e-8
-    
-    frequencies = np.minimum(np.array([frequency*(x+1) for x in range(len(factor))]), sample_rate//2)
-    amplitudes = np.array([amplitude*x for x in factor])
-    
+    """
+    assert abs(1 - sum(factor)) < 1e-8
+
+    frequencies = np.minimum(np.array([frequency * (x + 1) for x in range(len(factor))]), sample_rate // 2)
+    amplitudes = np.array([amplitude * x for x in factor])
+
     fundamental = get_sine_wave(frequencies[0], duration, sample_rate, amplitudes[0])
     for i in range(1, len(factor)):
         overtone = get_sine_wave(frequencies[i], duration, sample_rate, amplitudes[i])
         fundamental += overtone
     return fundamental
 
+
 def get_adsr_weights(frequency, duration, length, decay, sustain_level, sample_rate=44100):
-    '''
+    """
     ADSR(attack, decay, sustain, and release) envelop generator with exponential
     weights applied.
 
@@ -106,7 +110,7 @@ def get_adsr_weights(frequency, duration, length, decay, sustain_level, sample_r
         List of fractions that indicates length of each stage in ADSR.
     decay : list
         List of float for decay factor to be used in each stage for exponential
-        weights. 
+        weights.
     sustain_level : float
         Amplitude of `S` stage as a fraction of max amplitude.
     sample_rate : int, optional
@@ -116,50 +120,51 @@ def get_adsr_weights(frequency, duration, length, decay, sustain_level, sample_r
     -------
     weights : ndarray
 
-    '''
-    assert abs(sum(length)-1) < 1e-8
-    assert len(length) ==len(decay) == 4
-    
-    intervals = int(duration*frequency)
-    len_A = np.maximum(int(intervals*length[0]),1)
-    len_D = np.maximum(int(intervals*length[1]),1)
-    len_S = np.maximum(int(intervals*length[2]),1)
-    len_R = np.maximum(int(intervals*length[3]),1)
-    
+    """
+    assert abs(sum(length) - 1) < 1e-8
+    assert len(length) == len(decay) == 4
+
+    intervals = int(duration * frequency)
+    len_A = np.maximum(int(intervals * length[0]), 1)
+    len_D = np.maximum(int(intervals * length[1]), 1)
+    len_S = np.maximum(int(intervals * length[2]), 1)
+    len_R = np.maximum(int(intervals * length[3]), 1)
+
     decay_A = decay[0]
     decay_D = decay[1]
     decay_S = decay[2]
     decay_R = decay[3]
-    
-    A = 1/np.array([(1-decay_A)**n for n in range(len_A)])
-    A = A/np.nanmax(A)
-    D = np.array([(1-decay_D)**n for n in range(len_D)])
-    D = D*(1-sustain_level)+sustain_level
-    S = np.array([(1-decay_S)**n for n in range(len_S)])
-    S = S*sustain_level
-    R = np.array([(1-decay_R)**n for n in range(len_R)])
-    R = R*S[-1]
-    
-    weights = np.concatenate((A,D,S,R))
-    smoothing = np.array([0.1*(1-0.1)**n for n in range(5)])
-    smoothing = smoothing/np.nansum(smoothing)
+
+    A = 1 / np.array([(1 - decay_A) ** n for n in range(len_A)])
+    A = A / np.nanmax(A)
+    D = np.array([(1 - decay_D) ** n for n in range(len_D)])
+    D = D * (1 - sustain_level) + sustain_level
+    S = np.array([(1 - decay_S) ** n for n in range(len_S)])
+    S = S * sustain_level
+    R = np.array([(1 - decay_R) ** n for n in range(len_R)])
+    R = R * S[-1]
+
+    weights = np.concatenate((A, D, S, R))
+    smoothing = np.array([0.1 * (1 - 0.1) ** n for n in range(5)])
+    smoothing = smoothing / np.nansum(smoothing)
     weights = np.convolve(weights, smoothing, mode='same')
-    
-    weights = np.repeat(weights, int(sample_rate*duration/intervals))
-    tail = int(sample_rate*duration-weights.shape[0])
+
+    weights = np.repeat(weights, int(sample_rate * duration / intervals))
+    tail = int(sample_rate * duration - weights.shape[0])
     if tail > 0:
-        weights = np.concatenate((weights, weights[-1]-weights[-1]/tail*np.arange(tail)))
+        weights = np.concatenate((weights, weights[-1] - weights[-1] / tail * np.arange(tail)))
     return weights
+
 
 def get_song_data(music_notes, note_values, factor, length, decay, sustain_level,
                   sample_rate=44100, amplitude=4096):
-    '''
-    Generate song from notes. 
+    """
+    Generate song from notes.
 
     Parameters
     ----------
     music_notes : list
-        List of note names. 
+        List of note names.
     note_values : list
         List of note duration.
     factor : list
@@ -179,14 +184,14 @@ def get_song_data(music_notes, note_values, factor, length, decay, sustain_level
     -------
     song : ndarray
 
-    '''
+    """
     note_freqs = get_piano_notes()
     frequencies = [note_freqs[note] for note in music_notes]
-    song = np.concatenate([apply_overtones(freq, val, factor) 
+    song = np.concatenate([apply_overtones(freq, val, factor)
                            for freq, val in zip(frequencies, note_values)])
-    weights = np.concatenate([get_adsr_weights(freq, val, length, decay, sustain_level) 
+    weights = np.concatenate([get_adsr_weights(freq, val, length, decay, sustain_level)
                               for freq, val in zip(frequencies, note_values)])
 
-    song = song*weights
-    song = song*(amplitude/np.max(song))
+    song = song * weights
+    song = song * (amplitude / np.max(song))
     return song
